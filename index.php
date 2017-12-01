6fc0813e7a79f9db4d50d28d7366b0c55af0352d
juliedi <julie.di@codeur.online>
2017-12-01 10:04:48 +0100

    dossier complet à jour

--- index.php
+++ index.php
@@ -2,39 +2,31 @@
 if(isset($_GET['route']))
 {
   $route = $_GET['route'];
-<<<<<<< HEAD
-
-  if(preg_match("#film/[0-9]#",$route)) {
-    
-  }
-=======
-  if(preg_match('|film/[0-9][0-9]|', $route) || preg_match('|film/[0-9]|', $route))
+  if(preg_match('|film/[0-9]|', $route) || preg_match('|film/[0-9][0-9]|', $route)) 
   {
     $route = explode( '/', $route);
     $idfilm = $route[1];
     include('controllers/filmsController.php');
     die();
   }
-
->>>>>>> ac71c9e61b14c3be413304604cf953ab73f2804a
 }
   if(empty($route) || !isset($_GET['route']))
   {
     $route = 'home';
   }
-  switch ($route)
-  {
-    case 'home':
-    include("controllers/homeController.php");
-    break;
+    switch ($route)
+    {
+      case 'home':
+      include("controllers/homeController.php");
+      break;
 
-    case 'film' :
-    include('controllers/filmsController.php');
-    break;
+      case 'film' :
+      include('controllers/filmsController.php');
+      break;
 
-    default:
-    echo "404";
-  }
+      default:
+      echo "404";
+    }
 ?>
 
 
